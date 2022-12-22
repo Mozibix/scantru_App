e57791a286aac45a0d31c88dcd7928d196bd6953
opusunju moses <opuzmoses@gmail.com>
2022-12-22 12:28:55 +0100

    first push of scantru codes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { Home } from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
       <div>
-        <Home />
+        <p>hello welcome to react app</p>{" "}
       </div>
     </div>
   );
